c6f70f5f1fcdd20a0e7d5d73aa2a7ce96db37d74
Natan__Brito <natanismaelbrito@gmail.com>
2022-04-02 00:54:14 -0300

    feito footer dinamico

--- src/components/hoje/Hoje.js
+++ src/components/hoje/Hoje.js
@@ -15,28 +15,44 @@ export default function Hoje(){
     const {token}= useContext(TokenContext);
     const {image}=useContext(ImageContext);
     const [feito,setFeito]=useState(0);
+    let habitoCompleto=0;
     const config = {
         headers: {
          Authorization: `Bearer ${token}`
         }
       }
+      for(let i=0;i<habitosDiario.length;i++){
+        if(habitosDiario[i].done ===true){
+           habitoCompleto++
+        }
+    }
+     
+     const porcentagem= habitoCompleto/habitosDiario.length;
+      console.log(porcentagem)
     useEffect(()=>{
         const Get="https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today";
         const promise= axios.get(Get,config)
         promise.then(Response =>{console.log(Response); setHabitosDiario(Response.data)})
         promise.catch(err=>{console.log("erro "+ err)})
     },[])
+    function HabitoTexto() {
+        if (habitoCompleto >0) {
+            setFooterNum(porcentagem*100)
+            return <p className="green">{`${(porcentagem * 100).toFixed(0)}% dos hábitos concluídos`}</p>
+        }
+        return <p className="">Nenhum hábito concluído ainda</p>
+      }
 
     return(
         <Container>
             <Header img={image}  />
              <DiaConcluido>
                 <span className="title">{dayjs().locale('pt-br').format('dddd, DD/MM')}</span>
-                <span className="subtitle">Nenhum hábito concluído ainda</span>
+                <span className="subtitle">{<HabitoTexto/>}</span> 
             </DiaConcluido>
             <ContainerHabito>
         {habitosDiario.map(habito=>{
-            console.log("sou o feito"+feito)
+           
             const {id,currentSequence,done,highestSequence,name}=habito;
              return(
         <HabitoCheck  callback={(value)=>{setHabitosDiario(value)}}
@@ -77,4 +93,7 @@ const DiaConcluido = styledComponents.div`
 const Container = styledComponents.div`
 margin-left:18px;
 margin-right:18px;
+.green{
+    color:green;
+}
 `;
